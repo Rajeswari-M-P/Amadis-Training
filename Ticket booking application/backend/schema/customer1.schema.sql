f26586036e11a6186ed3ab255eba1e977a2f4590
Rajeswari-M-P <131983364+Rajeswari-M-P@users.noreply.github.com>
2024-07-22 15:49:16 +0530

    Create customer1.schema.sql

---
+++ Ticket booking application/backend/schema/customer1.schema.sql
@@ -0,0 +1,7 @@
+
+-- Create the table customer1 in the Customer database
+CREATE TABLE customer1 (
+    username VARCHAR(50)  NOT NULL,
+    email VARCHAR(100)  NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
