816c4c499cee1c300dae2b9973edb6fea9e8138e
Rajeswari-M-P <131983364+Rajeswari-M-P@users.noreply.github.com>
2024-07-22 15:12:01 +0530

    Create bar.js

---
+++ Ticket booking application/Frontend/components/bar.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const imagebar = () => {
+  return (
+    <div className="imagebar">
+      <img src="/imagebar.avif" alt="Logo" className="imagebar-logo" />
+    </div>
+  );
+};
+
+export default imagebar;
